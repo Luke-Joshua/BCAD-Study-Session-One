6d0bd0bc68b0a397fcf86d9e9b0942cfe9e0aaf9
Luke Joshua <ST10511274@myemeris.edu.za>
2026-03-06 12:01:15 +0200

    I forgot to update the last study session
    1. Removed comments & Author tag
    2. Removed square number method to keep things simple
    3. Added a my age variable
    4. De-commented the order of operations

--- 2026-02-20 Hello World Study - name and colour/HelloWorldStudy/src/main/java/com/mycompany/helloworldstudy/HelloWorldStudy.java
+++ 2026-02-20 Hello World Study - name and colour/HelloWorldStudy/src/main/java/com/mycompany/helloworldstudy/HelloWorldStudy.java
@@ -1,34 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.helloworldstudy;
 
 import java.util.Scanner;
 
-
-
-
-/**
- *
- * @author Luke
- * 
- */
 public class HelloWorldStudy {
+    
+    public static int SquareNumber(int numberA, int powerTimes){
+        
+        return (int) Math.pow((double) numberA, (double) powerTimes);
+    }
 
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
         String myName = "";
         String favColour = "";
+        int myAge = 0;
         
         System.out.println("Hi - what is your name? ");//Question - 1
         myName = scan.nextLine();//2 -input
         System.out.println("Hello " + myName);//3
         
         
-        System.out.println("What is your favorite colour?");//Question - 4
-        favColour = scan.nextLine(); // 5  - input
-        System.out.println("Hello '" + myName + "', your fav colour is '" + favColour +"'" ); // 6
+        System.out.println("What is your favorite colour?");
+        favColour = scan.nextLine(); 
+        System.out.println("Hello '" + myName + "', your fav colour is '" + favColour +"'" );
+        
+        System.out.println("What is your age?");
+        myAge = scan.nextInt(); 
+        System.out.println("Hello '" + myName + "', your fav colour is '" + favColour +"'" );
     }
 }
